3c95f5c73f3f0b0c95546c56dabc701d4695352a
saadetsimsek <saadetsimsek13@gmail.com>
2024-08-05 13:41:05 +0300

    Initial Commit

---
+++ NSCacheUITests/NSCacheUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NSCacheUITestsLaunchTests.swift
+//  NSCacheUITests
+//
+//  Created by Saadet Şimşek on 05/08/2024.
+//
+
+import XCTest
+
+final class NSCacheUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
